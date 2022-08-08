824e3faf4a3282557fafebacee916226025c6c75
Guddee <guddee.n@homesfy.in>
2022-08-08 17:12:20 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -405,7 +405,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-
+}
 function showSlides1(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
